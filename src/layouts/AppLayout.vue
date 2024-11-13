d1776745c314e747832ef578e0809bb898087f2a
hutao <2827883762@qq.com>
2024-11-13 10:53:57 +0800

    init project

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue'
+
+const leftDrawerOpen = ref(false)
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
+
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="/src/assets/favicon.png">
+          </q-avatar>
+          <strong>Novo Force</strong>
+        </q-toolbar-title>
+        <q-btn flat round dense icon="person" />
+        <q-btn flat round dense icon="logout" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
