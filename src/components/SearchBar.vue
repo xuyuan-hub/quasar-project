aa4bedd189e29f6eecfa0cb6a488c38d3ca449c8
huihongyuan <2827883762@qq.com>
2024-11-14 09:32:52 +0800

    nothing

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,17 +1,28 @@
 <script setup>
 import { ref } from 'vue'
-const text = ref("")
+const uinput = ref("")
+const ufile = ref()
+
+const submitClicked = async () => {
+  console.log(uinput.value)
+  uinput.value = ""
+}
 
 </script>
 
 <template>
-  <q-input v-model="text" label="Prompt...">
-    <template v-slot:prepend>
-      <q-icon name="event"></q-icon>
-    </template>
-    <template v-slot:append>
-      <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
-      <q-icon name="search" />
-    </template>
-  </q-input>
+  <div>
+    <q-file inlined v-model="ufile">
+      <template v-slot:prepend>
+        <q-icon name="attach_file" />
+      </template>
+    </q-file>
+    <q-input v-model="uinput" label="Prompt...">
+      <template v-slot:append>
+        <q-icon v-if="uinput !== ''" name="close" @click="uinput = ''" class="cursor-pointer" />
+        <q-icon name="search" @click="submitClicked" />
+      </template>
+    </q-input>
+  </div>
+
 </template>
