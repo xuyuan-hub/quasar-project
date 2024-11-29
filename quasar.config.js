28ccf6495f68069254e769e1f3bdd1c77938b3ce
huihongyuan <2827883762@qq.com>
2024-11-29 15:37:50 +0800

    deploy2

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: "./",
+      // publicPath: "./",
       // analyze: true,
       // env: {},
       // rawDefine: {}
